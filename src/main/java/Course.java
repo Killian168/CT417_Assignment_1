caf843443cad52cd8df0b1c3f766850c9cd8d4b9
Killian O'Dalaigh <Killian.ODalaigh@genesys.com>
2020-10-13 14:05:34 +0100

    Course Tests written and passing, PDF added

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -1,4 +1,6 @@
 import org.joda.time.DateTime;
+
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
 
 public class Course {
@@ -6,54 +8,90 @@ public class Course {
     private String name;
     private ArrayList<Module> modules;
     private ArrayList<Student> students;
-    private DateTime start_dat;
+    private DateTime start_date;
     private DateTime end_date;
 
-    public Course(String name, ArrayList<Module> modules, ArrayList<Student> students, DateTime start_dat, DateTime end_date) {
+    ////////////// Constructors //////////////
+
+    public Course(String name, ArrayList<Module> modules, ArrayList<Student> students,
+                  DateTime start_date, DateTime end_date) {
+        this(name, modules, start_date, end_date);
+        this.setStudents(students);
+    }
+
+    public Course(String name, ArrayList<Module> modules, DateTime start_date, DateTime end_date) {
+        this.setName(name);
+        this.setModules(modules);
+        this.setStart_date(start_date);
+        this.setEnd_date(end_date);
+    }
+
+    ////////////// Setters //////////////
+
+    public void setName(String name) {
+        if (name == null || name.isBlank() || name.isEmpty()) {
+            throw new InvalidParameterException("Course name cannot be empty or null");
+        }
         this.name = name;
+    }
+
+    public void setModules(ArrayList<Module> modules) {
+        if (modules == null) {
+            throw new InvalidParameterException("Course modules cannot be set to null");
+        }
         this.modules = modules;
+    }
+
+    public void setStudents(ArrayList<Student> students) {
+        if (students == null) {
+            throw new InvalidParameterException("Course students cannot be set to null");
+        }
         this.students = students;
-        this.start_dat = start_dat;
+    }
+
+    public void setStart_date(DateTime start_date) {
+        if (start_date == null) {
+            throw new InvalidParameterException("Course start date cannot be null");
+        }
+        if (end_date != null && start_date.isAfter(end_date)) {
+            throw new InvalidParameterException("Course start date cannot be after course end date");
+        }
+        this.start_date = start_date;
+    }
+
+    public void setEnd_date(DateTime end_date) {
+        if (end_date == null) {
+            throw new InvalidParameterException("Course end date cannot be null");
+        }
+        if (start_date != null && end_date.isBefore(start_date)) {
+            throw new InvalidParameterException("Course end date cannot be before course start date");
+        }
         this.end_date = end_date;
     }
 
+    ////////////// Getters //////////////
+
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public ArrayList<Module> getModules() {
         return modules;
     }
 
-    public void setModules(ArrayList<Module> modules) {
-        this.modules = modules;
-    }
-
     public ArrayList<Student> getStudents() {
         return students;
     }
 
-    public void setStudents(ArrayList<Student> students) {
-        this.students = students;
-    }
-
-    public DateTime getStart_dat() {
-        return start_dat;
-    }
-
-    public void setStart_dat(DateTime start_dat) {
-        this.start_dat = start_dat;
+    public DateTime getStart_date() {
+        return start_date;
     }
 
     public DateTime getEnd_date() {
         return end_date;
     }
 
-    public void setEnd_date(DateTime end_date) {
-        this.end_date = end_date;
-    }
+    ////////////// Public methods //////////////
+
+    ////////////// Private methods //////////////
 }
--- src/test/java/CourseTest.java
+++ src/test/java/CourseTest.java
@@ -1,2 +1,141 @@
+import org.joda.time.DateTime;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class CourseTest {
+
+    private String testName = "testName";
+    private DateTime startDate = new DateTime();
+    private DateTime endDate = new DateTime();
+    public Course testCourse;
+
+    @BeforeEach
+    public void setUpTestCourse() {
+        String name = this.testName;
+        ArrayList<Module> modules = new ArrayList<>();
+        ArrayList<Student> students = new ArrayList<>();
+        DateTime start_date = this.startDate;
+        DateTime end_date = this.endDate;
+
+        this.testCourse = new Course(name, modules, students, start_date, end_date);
+    }
+
+    @Test 
+    @DisplayName("Test setName if null")
+    public void testSetNameNull() {
+        checkInvalidParamException(() -> {
+                testCourse.setName(null);
+        },"Course name cannot be empty or null");
+    }
+
+    @Test 
+    @DisplayName("Test setName if empty")
+    public void testSetNameEmpty() {
+        checkInvalidParamException(() -> {
+                testCourse.setName("");
+        },"Course name cannot be empty or null");
+    }
+
+    @Test 
+    @DisplayName("Test setName if blank")
+    public void testSetNameBlank() {
+        checkInvalidParamException(()->{
+                testCourse.setName("     ");
+        },"Course name cannot be empty or null");
+    }
+
+    @Test
+    @DisplayName("Test setModules if null")
+    public void testSetModulesNull() {
+        checkInvalidParamException(()->{
+            testCourse.setModules(null);
+        },"Course modules cannot be set to null");
+    }
+
+    @Test
+    @DisplayName("Test setStudents if null")
+    public void testSetStudentsNull() {
+        checkInvalidParamException(()->{
+            testCourse.setStudents(null);
+        },"Course students cannot be set to null");
+    }
+
+    @Test
+    @DisplayName("Test setStartData if null")
+    public void testStartDateNull() {
+        checkInvalidParamException(()->{
+            testCourse.setStart_date(null);
+        },"Course start date cannot be null");
+    }
+
+    @Test
+    @DisplayName("Test setStartData if after end date")
+    public void testStartDateAfterEndDate() {
+        checkInvalidParamException(()->{
+            testCourse.setEnd_date(new DateTime(2020, 12, 22, 12, 12));
+            testCourse.setStart_date(new DateTime(2021, 12, 22, 12, 12));
+        },"Course start date cannot be after course end date");
+    }
+
+    @Test
+    @DisplayName("Test setEndDate if null")
+    public void testEndDateNull() {
+        checkInvalidParamException(()->{
+            testCourse.setEnd_date(null);
+        },"Course end date cannot be null");
+    }
+
+    @Test
+    @DisplayName("Test setEndDate if before start date")
+    public void testEndDateAfterEndDate() {
+        checkInvalidParamException(()->{
+            testCourse.setEnd_date(new DateTime(2021, 12, 22, 12, 12));
+            testCourse.setStart_date(new DateTime(2020, 12, 22, 12, 12));
+            testCourse.setEnd_date(new DateTime(2019, 12, 22, 12, 12));
+        },"Course end date cannot be before course start date");
+    }
+
+    @Test
+    @DisplayName("Test getName")
+    public void getName() {
+        assertEquals(testCourse.getName(), this.testName);
+    }
+
+    @Test
+    @DisplayName("Test getModules")
+    public void getModules() {
+        assertEquals(testCourse.getModules(), new ArrayList<>());
+    }
+
+    @Test
+    @DisplayName("Test getStudents")
+    public void getStudents() {
+        assertEquals(testCourse.getStudents(), new ArrayList<>());
+    }
+
+    @Test
+    @DisplayName("Test getStart_date")
+    public void getStart_date() {
+        assertEquals(testCourse.getStart_date(), this.startDate);
+    }
+
+    @Test
+    @DisplayName("Test getEnd_date")
+    public void getEnd_date() {
+        assertEquals(testCourse.getEnd_date(), this.endDate);
+    }
+
+    public void checkInvalidParamException(Runnable courseMethod, String exceptionMessage) {
+        Exception exception = assertThrows(InvalidParameterException.class, courseMethod::run);
+        String actualMessage = exception.getMessage();
+        assertTrue(actualMessage.contains(exceptionMessage));
+    }
 }
